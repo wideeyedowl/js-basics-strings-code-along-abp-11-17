9f166951c6dbe63ce6b4e042908ddb5d5dcd0382
Prasitha Thomas <prasitha.thomas@gmail.com>
2017-11-11 18:57:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 const currentUser='Ada Lovelace';
 const welcomeMessage= `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage=welcomeMessage.toUpperCase();
-const shortGreeting='Welcome, ${currentUser.slice(0)}!';
+const shortGreeting=`Welcome, ${currentUser.slice(0,1)}!`;
